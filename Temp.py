ff2d12433afa27767bb5d7bbc1d8f9f58ff56877
albert9052 <35403151+albert9052@users.noreply.github.com>
2021-10-03 16:03:41 +0800

    Ensure code functionality.

--- Temp.py
+++ Temp.py
@@ -9,16 +9,19 @@ from keras.layers import LSTM, Dense
 import requests
 import datetime
 import json
-
+look_back = 30
 app = Flask(__name__)
 
 model = keras.models.load_model('ALLSKY_SFC_PAR_TOT_10021210.h5')
 
 print("HI")
 
-def predict(num_prediction, model):
+def predict(num_prediction, model, allskypar):
+    #allskypar = np.ndarray(allskypar)
+    #allskypar = allskypar.reshape(-1)
+    
     prediction_list = allskypar[-look_back:]
-
+    print(prediction_list)
     for _ in range(num_prediction):
         x = prediction_list[-look_back:]
         x = x.reshape((1, look_back, 1))
@@ -31,11 +34,11 @@ def predict(num_prediction, model):
 def predict_dates(num_prediction):
     last_date = df['INDEX'].values[-1]
     prediction_dates = pd.date_range(last_date, periods=num_prediction+1).tolist()
-    return prediction_dates
+    return ""
 
 @app.route("/")
 def hello():
-    return "Hello, World1!"
+    return ""
 
 @app.route('/api', methods =['GET'])
 def api():
@@ -47,19 +50,30 @@ def api():
     URL = 'https://power.larc.nasa.gov/api/temporal/daily/point?parameters=ALLSKY_SFC_PAR_TOT&community=SB&longitude=' + lng + '&latitude=' + lat + '&start=' + veryLongAgo.strftime('%Y%m%d') + '&end=' + today.strftime('%Y%m%d') + '&format=JSON'
     print(URL)
 
+    num_prediction = int(days)
+
     r = requests.get(url = URL)
     print('get')
-    data = r.json
-
+    data = r.json()['properties']['parameter']['ALLSKY_SFC_PAR_TOT']
     print('ohno')
     print(data)
-    
-    print(json.dumps(data))
 
-    forecast = predict(days, model)
+    allskypar_array = []
+    for key in data:
+            allskypar_array.append(int(data[key]))
+    print(allskypar_array)
+    allskypar = pd.DataFrame(allskypar_array)
+    print(allskypar)
+    asp = allskypar.to_numpy()
+    forecast = predict(num_prediction, model, asp)
+    print(forecast)
+    res = 0
+    for i in range(num_prediction):
+        res+=forecast[i]
+    return str(res)
 
-    return "Hello" + json.dumps(data)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run('0.0.0.0', '5000')
+    app.run()
+    #app.run('0.0.0.0', '5000')
